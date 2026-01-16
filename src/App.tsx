a979c96834d42372914bc7e968ddb32f81a2e88e
Mak <mak.terbovc@gmail.com>
2026-01-16 18:24:50 +0100

    added contact icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import "./App.scss";
 import LogoSmall from "./assets/icons/logo-small.svg?react";
+import PhoneIcon from "./assets/icons/phone.svg?react";
+import EmailIcon from "./assets/icons/email.svg?react";
+import InstagramIcon from "./assets/icons/instagram.svg?react";
 
 function App() {
   return (
     <footer className="footer">
       <div className="footer-title">
         <LogoSmall className="logo" />
-        <h1 className="title-text">
+        <h1 className="footer-title-text">
           PRIPREME ZA PRIJEMNE ISPITE IZ MATEMATIKE
         </h1>
       </div>
