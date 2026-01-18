d40336cc149b576d56f75418abf48a74ed0644f5
Mak <mak.terbovc@gmail.com>
2026-01-18 11:51:24 +0100

    added footer nesting, changed text transform into uppercase

--- src/App.tsx
+++ src/App.tsx
@@ -7,35 +7,35 @@ import InstagramIcon from "./assets/icons/instagram.svg?react";
 function App() {
   return (
     <footer className="footer">
-      <div className="footer-title">
+      <div className="logo-wrapper">
         <LogoSmall className="logo" />
-        <h1 className="footer-title-text">
-          PRIPREME ZA PRIJEMNE <br /> ISPITE IZ MATEMATIKE
+        <h1 className="subtitle">
+          Pripreme za prijemne <br /> ispite iz matematike
         </h1>
       </div>
-      <div className="footer-content">
-        <div className="footer-contacts">
-          <h2 className="footer-h2">KONTAKT</h2>
-          <div className="contact-items">
+      <div className="content">
+        <div className="contacts">
+          <h2 className="label">Kontakt</h2>
+          <div className="item">
             <div className="contact">
               <div className="icon-div">
-                <PhoneIcon className="contact-icon" />
+                <PhoneIcon className="icon" />
               </div>
-              <span className="footer-text">098 923 4897</span>
+              <span className="text">098 923 4897</span>
             </div>
 
             <div className="contact">
               <div className="icon-div">
-                <EmailIcon className="contact-icon" />
+                <EmailIcon className="icon" />
               </div>
-              <span className="footer-text">matelierpripreme@gmail.com</span>
+              <span className="text">matelierpripreme@gmail.com</span>
             </div>
 
             <div className="contact">
               <div className="icon-div">
-                <InstagramIcon className="contact-icon" />
+                <InstagramIcon className="icon" />
               </div>
-              <span className="footer-text">@matelier_pripreme</span>
+              <span className="text">@matelier_pripreme</span>
             </div>
           </div>
         </div>
