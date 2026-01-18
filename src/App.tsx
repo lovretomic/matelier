491c1e134a8bd1bff5ff54434a26cfcd6bc7f62c
Lovre Tomić <92167015+lovretomic@users.noreply.github.com>
2026-01-18 16:52:06 +0100

    added sections

--- src/App.tsx
+++ src/App.tsx
@@ -6,58 +6,68 @@ import InstagramIcon from "./assets/icons/instagram.svg?react";
 
 function App() {
   return (
-    <footer className="footer">
-      <div className="logo-wrapper">
-        <LogoSmall className="logo" />
-        <h1 className="subtitle">
-          Pripreme za prijemne <br /> ispite iz matematike
-        </h1>
-      </div>
+    <>
+      <header className="header"></header>
+      <section className="hero"></section>
+      <section className="why"></section>
+      <section className="methods"></section>
+      <section className="goal"></section>
+      <section className="who"></section>
+      <section className="packages"></section>
+      <section className="location"></section>
+      <footer className="footer">
+        <div className="logo-wrapper">
+          <LogoSmall className="logo" />
+          <h1 className="subtitle">
+            Pripreme za prijemne <br /> ispite iz matematike
+          </h1>
+        </div>
 
-      <div className="contacts">
-        <h2 className="label">Kontakt</h2>
-        <div className="items-wrapper">
-          <div
-            className="item"
-            onClick={() => {
-              window.location.href = "tel:0989234897";
-            }}
-          >
-            <div className="icon-div">
-              <PhoneIcon className="icon" />
+        <div className="contacts">
+          <h2 className="label">Kontakt</h2>
+          <div className="items-wrapper">
+            <div
+              className="item"
+              onClick={() => {
+                window.location.href = "tel:0989234897";
+              }}
+            >
+              <div className="icon-div">
+                <PhoneIcon className="icon" />
+              </div>
+              <span className="text">098 923 4897</span>
             </div>
-            <span className="text">098 923 4897</span>
-          </div>
 
-          <div
-            className="item"
-            onClick={() => {
-              window.location.href = "mailto:matelierpripreme@gmail.com";
-            }}
-          >
-            <div className="icon-div">
-              <EmailIcon className="icon" />
+            <div
+              className="item"
+              onClick={() => {
+                window.location.href = "mailto:matelierpripreme@gmail.com";
+              }}
+            >
+              <div className="icon-div">
+                <EmailIcon className="icon" />
+              </div>
+              <span className="text">matelierpripreme@gmail.com</span>
             </div>
-            <span className="text">matelierpripreme@gmail.com</span>
-          </div>
 
-          <div
-            className="item"
-            onClick={() => {
-              window.open(
-                "https://www.instagram.com/matelier_pripreme/",
-                "_blank",
-              );
-            }}
-          >
-            <div className="icon-div">
-              <InstagramIcon className="icon" />
+            <div
+              className="item"
+              onClick={() => {
+                window.open(
+                  "https://www.instagram.com/matelier_pripreme/",
+                  "_blank",
+                );
+              }}
+            >
+              <div className="icon-div">
+                <InstagramIcon className="icon" />
+              </div>
+              <span className="text">@matelier_pripreme</span>
             </div>
-            <span className="text">@matelier_pripreme</span>
           </div>
         </div>
-      </div>
-    </footer>
+      </footer>
+    </>
   );
 }
 
