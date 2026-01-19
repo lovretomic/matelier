260cc4d7966b34297d48bf02a42ba30562b11e51
lovretomic <92167015+lovretomic@users.noreply.github.com>
2026-01-19 16:54:40 +0100

    added map wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import PhoneIcon from "./assets/icons/phone.svg?react";
 import EmailIcon from "./assets/icons/email.svg?react";
 import InstagramIcon from "./assets/icons/instagram.svg?react";
 
+import LocationPinIcon from "./assets/icons/location-pin.svg?react";
+
 function App() {
   return (
     <>
@@ -14,7 +16,24 @@ function App() {
       <section className="goal"></section>
       <section className="who"></section>
       <section className="packages"></section>
-      <section className="location"></section>
+      <section className="location">
+        <div className="map-wrapper">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d437.7662589974481!2d15.970276874384956!3d45.812043529664045!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4765d6e335d05a35%3A0x8e7b0e973cb2dfc7!2sOsnovna%20%C5%A1kola%20Josipa%20Jurja%20Strossmayera!5e0!3m2!1sen!2shr!4v1768836864912!5m2!1sen!2shr"
+            width="600"
+            height="450"
+            style={{ border: 0 }}
+            loading="lazy"
+          />
+          <div className="address-wrapper">
+            <LocationPinIcon className="icon" />
+            <span className="address">
+              Varšavska 18, 10000 Zagreb
+              <span className="secondary"> (OŠ Josipa Jurja Strossmayera)</span>
+            </span>
+          </div>
+        </div>
+      </section>
       <footer className="footer">
         <div className="logo-wrapper">
           <LogoSmall className="logo" />
@@ -55,7 +74,7 @@ function App() {
               onClick={() => {
                 window.open(
                   "https://www.instagram.com/matelier_pripreme/",
-                  "_blank",
+                  "_blank"
                 );
               }}
             >
