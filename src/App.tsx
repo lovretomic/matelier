4bf1fb57afeaedb8703e23b8956f5a3305578869
lovretomic <92167015+lovretomic@users.noreply.github.com>
2026-01-28 18:06:57 +0100

    added toast message on desktop

--- src/App.tsx
+++ src/App.tsx
@@ -51,6 +51,7 @@ import Card from "./components/Card";
 import MobileMenu from "./components/MobileMenu";
 import Popup from "./components/Popup";
 import StickyNote from "./components/StickyNote";
+import toast, { Toaster } from "react-hot-toast";
 
 function TeacherPopupContent({ teacher }: { teacher: Teacher }) {
   const ref = useRef<HTMLDivElement>(null);
@@ -147,15 +148,16 @@ function App() {
     } else {
       try {
         await navigator.clipboard.writeText(phoneNumber);
-        alert("Phone number copied 📋");
-      } catch {
-        alert("Failed to copy phone number");
+        toast.success("Broj mobitela kopiran u međuspremnik 📋");
+      } catch (error) {
+        console.error("Failed to copy phone number: ", error);
       }
     }
   };
 
   return (
     <>
+      <Toaster />
       <MobileMenu
         isOpen={isMobileMenuOpen}
         sections={sections}
